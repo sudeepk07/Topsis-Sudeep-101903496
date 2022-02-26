6ce9cdace544a7a53f0d5552bbf59ee846214231
sudeepk07 <56503751+sudeepk07@users.noreply.github.com>
2022-02-26 21:52:11 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## Topsis_Tewatia
+
+# TOPSIS
+
+Submitted By: **Sudeep Singh - 101903496**.
+
+Type: **Package**.
+
+Title: **TOPSIS score and ranking**.
+
+Version: **0.0.1**.
+
+Description: **Evaluation of alternatives based on multiple criteria using TOPSIS method.**.
+
+---
+
+## What is TOPSIS?
+
+**T**echnique for **O**rder **P**reference by **S**imilarity to **I**deal **S**olution
+(TOPSIS) originated in the 1980s as a multi-criteria decision making method.
+TOPSIS chooses the alternative of shortest Euclidean distance from the ideal solution,
+and greatest distance from the negative-ideal solution.
+
+<br>
+
+## How to install this package:
+
+```
+>> pip install Topsis-Sudeep-101903496
+```
+
+### In Command Prompt
+
+```
+>> topsis data.csv "1,1,1,1,2" "+,+,-,+,-" result.csv
+```
+"# Topsis-Sudeep-101903496" 
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Topsis-Sudeep-101903496",
+    version="0.0.1",
+    author="Sudeep Singh",
+    author_email="singhsudeep0202@gmail.com",
+    description="A package for calculating Topsis Score and Rank them accordingly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/sudeepk07/Topsis-Sudeep-101903496.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["Topsis-Sudeep-101903496"],
+    include_package_data=True,
+    install_requires='pandas',
+    entry_points={
+        "console_scripts": [
+            "topsis=Topsis-Sudeep-101903496.topsis:main",
+        ]
+    },
+)
